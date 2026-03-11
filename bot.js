d098a3d8dae8cb02685a0071b31614178aac0914
073vitin <vsena3231@gmail.com>
2026-03-11 10:01:12 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,3 +98,13 @@ console.log("Servidor web rodando")
 })
 
 startBot()
+const express = require("express")
+const app = express()
+
+app.get("/", (req,res)=>{
+res.send("BotZap rodando")
+})
+
+app.listen(3000, ()=>{
+console.log("Servidor rodando na porta 3000")
+})
